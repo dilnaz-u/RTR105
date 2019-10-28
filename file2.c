1956927f19bf3504f8a44f2072a1b442076404d7
user <dilnaz-u@mail.ru>
2019-10-28 14:08:16 +0200

    20191028_14_08

---
+++ file2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+ {
+ float a=1.23456, b=7.89012, c=34.5678;
+
+ printf ("floats: %5e %5e %4e \n", 1.2, 7.89012, 34.5678);
+ printf("floats: %6e %6e %6e \n", 1.23, 7.8901, 34.56780);
+ printf("floats: %6e %6e %6e \n", 1.23456, 7.890, 34.567800);
+ }
