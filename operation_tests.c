e08c741116aa5c8c8ee16b93e22fda03a2d53ed1
user <dilnaz-u@mail.ru>
2019-11-11 12:03:04 +0200

    operation_tests.c_20191111_12_00

--- operation_tests.c
+++ operation_tests.c
@@ -3,9 +3,18 @@
 void main(){
  int a=7, b=2;
 
-printf("%d + %d = %d (sizeof \"+\" %d byte(s))\n",a,b,a+b,sizeof(a+b));
-printf("%d - %d = %d (sizeof \"-\" %d byte(s))\n",a,b,a-b,sizeof(a-b));
-printf("%d * %d = %d (sizeof \"*\" %d byte(s))\n",a,b,a*b,sizeof(a*b));
-printf("%d / %d = %d (sizeof \"/\" %d byte(s))\n",a,b,a+b,sizeof(a/b));
+printf("%d + %d = %d (sizeof \"a+b\" %lu byte(s))\n",a,b,a+b,sizeof(a+b));
+printf("%d - %d = %d (sizeof \"a-b\" %lu byte(s))\n",a,b,a-b,sizeof(a-b));
+printf("%d * %d = %d (sizeof \"a*b\" %lu byte(s))\n",a,b,a*b,sizeof(a*b));
+printf("%d / %d = %d (sizeof \"a/b\" %lu byte(s))\n",a,b,a/b,sizeof(a/b));
+
+printf("%d / %d = %.1f (result of using (float)(a/b) even with f format)\n",a,b,(float)(a/b));
+printf("%d / %d = %.1f (result of using (float)a/b)\n",a,b,(float)a/b);
+
+printf("%d %c %d = %d (sizeof \"a%cb\" %lu byte(s))\n",a,'%',b,a%b,'%',sizeof(a%b));
+
+printf("%d ++ = %d (sizeof \"a++\" %lu byte(s))\n",a,a++,sizeof(a++));
+printf("++%d = %d (sizeof \"++a\" %lu byte(s))\n",a,++a,sizeof(++a));
 
 
+}
