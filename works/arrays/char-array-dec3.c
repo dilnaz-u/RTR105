4687004438154f0c2e433d7289aae4e84be02393
user <dilnaz-u@mail.ru>
2019-12-09 14:06:16 +0200

    works__20191209_14_04

---
+++ works/arrays/char-array-dec3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+ int i;
+ char mas[100];
+
+ for(i=0;i<100;i++){
+  mas[i]=i+1;
+  printf("%d %d %c\n",i, mas[i], mas[i]);
+  }
+
+ printf("%p\n",mas);
+ printf("%p\n",&mas[0]);
+}
